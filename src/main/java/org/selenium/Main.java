394cee9af500e2f880c19301669438713599510f
Sebastian Manrique <a2018609@pucp.edu.pe>
2024-04-04 12:23:10 -0500

    Extraccion de datos

--- src/main/java/org/selenium/Main.java
+++ src/main/java/org/selenium/Main.java
@@ -7,17 +7,16 @@ import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.By;
-
-
 import java.io.FileReader;
 import java.time.Duration;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
 
         //Leer JSON
-        JSONObject jsonObj = new JSONObject();
+        JSONObject jsonObj;
         try {
             JSONParser parser = new JSONParser();
             Object object = parser.parse(new FileReader("src/main/resources/datos.json"));
@@ -33,10 +32,35 @@ public class Main {
 
         //Chrome Browser
         driver.get(url);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         WebElement domainInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ctl00_ContentPlaceHolder1_ucToolhandler_txtToolInput")));
         domainInput.sendKeys(domain);
         WebElement senderButton = driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucToolhandler_btnAction"));
         senderButton.click();
+
+        //Esperar que cargue la tabla
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("tool-result-table")));
+
+        //Extraer IP
+        List<WebElement> ipDomain = driver.findElements(By.xpath("//strong"));
+
+        //Extraer Blacklist
+        List<WebElement> blacklist = driver.findElements(By.xpath("//table[contains(@class,'tool-result-table')]//tr//td[2]"));
+
+        //Extraer LISTED
+        List<WebElement> namesListed = driver.findElements(By.xpath("//table[contains(@class,'tool-result-table')]//td[contains(text(),'LISTED')]/following-sibling::td[1]"));
+
+        //Imprimir datos
+        System.out.println("* IP DOMAIN: "+ipDomain.get(1).getText());
+        System.out.println("* Number of blacklists where the check was performed: "+blacklist.size());
+        System.out.println("* Number of Blacklists where it is listed: "+namesListed.size());
+        System.out.println("* Blacklists where it is listed: ");
+        for(WebElement element:namesListed){
+            System.out.println(element.getText());
+        }
+
+        //Cerrar driver
+        driver.quit();
+
     }
 }

4b50a9d1a7a680d558cd37a92c41971da4e74420
Sebastian Manrique <a2018609@pucp.edu.pe>
2024-04-04 10:10:57 -0500

    WebDriver agregado

--- src/main/java/org/selenium/Main.java
+++ src/main/java/org/selenium/Main.java
@@ -1,6 +1,12 @@
 package org.selenium;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        //Chrome Browser
+        driver.get("https://mxtoolbox.com/blacklists.aspx");
     }
 }

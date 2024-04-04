94f49034735a8e23e8a3002e1e42d30c8beafff0
Sebastian Manrique <a2018609@pucp.edu.pe>
2024-04-04 10:32:13 -0500

    Leer JSON

--- src/main/java/org/selenium/Main.java
+++ src/main/java/org/selenium/Main.java
@@ -1,12 +1,30 @@
 package org.selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
 
+        //Leer JSON
+        JSONObject jsonObj = new JSONObject();
+        try {
+            JSONParser parser = new JSONParser();
+            Object object = parser.parse(new FileReader("src/main/resources/datos.json"));
+            jsonObj = (JSONObject) object;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        //Extraer data del JSON
+        String url = jsonObj.get("url").toString();
+        String domain = jsonObj.get("domain").toString();
+
         //Chrome Browser
-        driver.get("https://mxtoolbox.com/blacklists.aspx");
+        driver.get(url);
     }
 }
